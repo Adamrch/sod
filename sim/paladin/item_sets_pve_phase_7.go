c4eed054f3ba79dfded20cf897b35b4fbb0ffc62
Adam Chodaba <adamrch44@gmail.com>
2025-01-18 04:52:55 -0500

    make t3 6pc work with wrath and multiplicative

--- sim/paladin/item_sets_pve_phase_7.go
+++ sim/paladin/item_sets_pve_phase_7.go
@@ -80,13 +80,20 @@ func (paladin *Paladin) applyNaxxramasRetribution6PBonus() {
 				oldApplyEffects := spell.ApplyEffects
 				spell.ApplyEffects = func(sim *core.Simulation, target *core.Unit, spell *core.Spell) {
 					critChanceBonus := 0.0
-					if target.MobType == proto.MobType_MobTypeUndead {
-						critChanceBonus = paladin.GetStat(stats.SpellCrit) / 100
+
+					if spell.SpellCode == SpellCode_PaladinExorcism && target.MobType == proto.MobType_MobTypeUndead {
+						critChanceBonus = 1.0
+					} else if target.MobType == proto.MobType_MobTypeUndead {
+						if paladin.hasRune(proto.PaladinRune_RuneHeadWrath) {
+							critChanceBonus = (paladin.GetStat(stats.SpellCrit) + paladin.GetStat(stats.MeleeCrit)) / 100
+						} else {
+							critChanceBonus = paladin.GetStat(stats.SpellCrit) / 100
+						}
 					}
 
-					spell.DamageMultiplierAdditive += critChanceBonus
+					spell.DamageMultiplier *= (1.0 + critChanceBonus)
 					oldApplyEffects(sim, target, spell)
-					spell.DamageMultiplierAdditive -= critChanceBonus
+					spell.DamageMultiplier /= (1.0 + critChanceBonus)
 				}
 			}
 		},
